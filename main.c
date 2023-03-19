da05faa40569da0cfea354e1eb9282b574252ff1
Beloin Sena <beloin.rodrigues@gmail.com>
2023-03-19 11:52:01 -0300

    Creating simple triangle

--- main.c
+++ main.c
@@ -2,6 +2,27 @@
 #include "test/test.h"
 #include <glad.h>
 #include <GLFW/glfw3.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define HEIGHT 800
+#define WIDTH 800
+
+// Vertex Shader source code
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+//Fragment Shader source code
+const char *fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 FragColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+                                   "}\n\0";
+
 
 GLFWwindow *setup();
 
@@ -12,43 +33,89 @@ void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods
 int main() {
     GLFWwindow *window = setup();
 
-    float triangle[] = {
-            0.4f, .5f, 0.f,
-            -.1f, -.5f, 0.f,
-            .9f, -.5f, 0.f,
+
+    gladLoadGL();
+    glViewport(0, 0, HEIGHT, WIDTH);
+    glClearColor(0.07f, 0.13f, 0.17f, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    GLfloat vertices[] = {
+            -0.5f, -0.5f * ((float) sqrt(3)) / 3, .0f,
+            0.5f, -0.5f * ((float) sqrt(3)) / 3, 0.0f,
+            0.0f, 0.5f * ((float) sqrt(3)) * 2 / 3, 0.0f
     };
 
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+    GLuint VAO, VBO;
+
+    glGenVertexArrays(1, &VAO);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBindVertexArray(VAO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, NULL);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.07f, 0.13f, 0.17f, 1);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // First triangle
-        GLuint v;
-        glGenBuffers(1, &v);
-        glBindBuffer(GL_ARRAY_BUFFER, v);
 
-        GLsizeiptr i = sizeof(triangle) * (sizeof(triangle) / sizeof(triangle[0]));
-        glBufferData(GL_ARRAY_BUFFER, i, &triangle[0], GL_STATIC_DRAW);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, NULL);
-
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    printf("Hello, World!\n");
-    PrintSmoll();
+
+    glDeleteVertexArrays(1, &VBO);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteProgram(shaderProgram);
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
 
 GLFWwindow *setup() {
     glfwInit();
-    GLFWwindow *window = glfwCreateWindow(640, 320, "OpenGL", NULL, NULL);
+
+    // Why doesn't work?
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+//    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(HEIGHT, WIDTH, "OpenGL", NULL, NULL);
+    if (window == NULL) {
+        exit(1);
+    }
+
     glfwMakeContextCurrent(window);
-    gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
-    glViewport(0, 0, 640, 320);
     glfwSetKeyCallback(window, keyCallback);
     return window;
 }
